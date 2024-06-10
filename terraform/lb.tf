9b1208c8d3f1f6a31c61f3dd49fc104676b34934
ashwireddy <145135466+ashwireddy@users.noreply.github.com>
2024-06-10 20:42:46 +0530

    Update lb.tf

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -1,25 +1,24 @@
-resource "aws_lb" "hello_world" {
+resource "aws_lb" "hello_world_lb" {
   name               = "hello-world-lb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.node_sg.id]
-  subnets            = [aws_subnet.public-sub.id]
+  subnets            = "subnet-01942ecd801bdd9d6"
 
   enable_deletion_protection = false
-  
+
 
    depends_on = [
-    aws_subnet.public-sub,
     aws_security_group.node_sg,
     aws_ecs_service.hello_world
   ]
 }
 
-resource "aws_lb_target_group" "hello_world" {
-  name     = "hello-world-tg"
+resource "aws_lb_target_group" "hello_world_node-tg" {
+  name     = "hello-world-node-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.node_vpc.id
+  vpc_id   = "vpc-0fdf12c7f24b737c1"
 
   health_check {
     path = "/"
@@ -27,18 +26,18 @@ resource "aws_lb_target_group" "hello_world" {
 }
 
 resource "aws_lb_listener" "hello_world" {
-  load_balancer_arn = aws_lb.hello_world.arn
+  load_balancer_arn = aws_lb.hello_world_lb.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.hello_world.arn
+    target_group_arn = aws_lb_target_group.hello_world_node-tg.arn
   }
 }
 
 resource "aws_lb_target_group_attachment" "hello_world" {
-  target_group_arn = aws_lb_target_group.hello_world.arn
+  target_group_arn = aws_lb_target_group.hello_world_node-tg.arn
   target_id        = aws_ecs_service.hello_world.id
   port             = 80
 }
