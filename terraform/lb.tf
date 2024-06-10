c51cd31b270d7be4da80785413706d5eb9328e62
ashwireddy <145135466+ashwireddy@users.noreply.github.com>
2024-06-10 22:46:58 +0530

    Update lb.tf

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "hello_world_lb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.node_sg.id]
-  subnets            = "subnet-01942ecd801bdd9d6"
+  subnets            = ["subnet-01942ecd801bdd9d6", "subnet-054663795c7db4d40"]
 
   enable_deletion_protection = false
 
